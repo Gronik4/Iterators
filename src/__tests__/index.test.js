4b639886a2fc59c8800f498bb93019a2a82501e9
Gronik64 <golegni@yandex.ru>
2021-12-06 18:10:54 +0300

    Правки по замечаниям

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,26 +1,25 @@
 import Team from '../index';
 
-const char = {
-  name: 'Лучник',
-  type: 'Bowman',
-  health: 50,
-  level: 1,
-  attack: 40,
-  defence: 10,
-};
-
-const daem = {
-  name: 'Демон',
-  type: 'Daemon',
-  health: 40,
-  level: 1,
-  attack: 10,
-  defence: 40,
-};
-
-const end = { done: true, value: undefined };
-
 test('Test Iterator', () => {
+  const char = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  
+  const daem = {
+    name: 'Демон',
+    type: 'Daemon',
+    health: 40,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  };
+  
+  const end = { done: true, value: undefined };
   const team = new Team(char, daem).iterator();
   expect(team.next().value).toEqual(char);
   expect(team.next().value).toEqual(daem);
